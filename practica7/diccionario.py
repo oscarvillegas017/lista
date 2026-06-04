8f83732c5551228c175cb547b0d2621379567fac
oscarvillegas017 <4659599@clases.edu.sv>
2026-06-04 09:41:53 -0600

    Create diccionario.py

---
+++ practica7/diccionario.py
@@ -0,0 +1,17 @@
+# lista de elementos llave:valor
+# arreglos asociativos
+
+persona = {
+    "nombre": "Rodrigo",
+    "edad": 800,
+    "apellido": "montemayor"
+}
+
+persona["apodo"] = "Ringa tech"
+
+print(persona.items())
+
+print("direccion" in persona)
+
+# for key, value in persona.items():
+#     print(f"la llave {key} tiene valor {value}")
