041be7f78840522fb4f0aa7d04a5d51906d727fd
oscarvillegas017 <4659599@clases.edu.sv>
2026-06-04 09:47:47 -0600

    Create lista.py

---
+++ practica11/lista.py
@@ -0,0 +1,8 @@
+listas=["Rodrigo","Juan"]#constantemente agregando información
+tupla=("Rodrigo","Juan")
+
+videojuego = []
+#nombres, Genero, años
+videojuego.append(("final fantasy 7", "JRPG", 1997))
+videojuego.append(("outer wilds","aventura",2019))
+print(videojuego)
