5eaa0d643ea7e0a14f73b13fe99752c68d1acdb7
oscarvillegas017 <4659599@clases.edu.sv>
2026-06-04 09:44:48 -0600

    Create funciones2.py

---
+++ practica9/funciones2.py
@@ -0,0 +1,7 @@
+def saludar_y_sumar(nombre,num1,num2=0):
+    print("saludos", nombre)
+    print("resultado de la suma",num1+num2)
+
+    saludar_y_sumar("Rodrigo",10,5)
+    saludar_y_sumar("juan",2)
+    saludar_y_sumar("pedro",800,500)
