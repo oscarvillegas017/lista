30d6a091c09347e6e5beef43e294bf1a650f665e
oscarvillegas017 <4659599@clases.edu.sv>
2026-06-04 09:33:32 -0600

    Create ciclo.py

---
+++ ciclo.py
@@ -0,0 +1,18 @@
+# ciclo interracion, bucle
+
+# while
+
+i = 0
+
+while i < 10:
+    if i < 5:
+        print("el numero", i, "es menor a 5")
+    else:
+        print("el numero", i, "es mayor o igual a 5")
+
+    i += 1
+
+print("termino la interaccion")
+
+# for x in range(1,11):
+#     print(x)
