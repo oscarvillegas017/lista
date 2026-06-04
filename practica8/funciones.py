ee78c48118fb7edb25115bdd3517ee1951ac5efa
oscarvillegas017 <4659599@clases.edu.sv>
2026-06-04 09:43:16 -0600

    Create funciones.py

---
+++ practica8/funciones.py
@@ -0,0 +1,16 @@
+print("Hola", nombre)
+
+"""
+saludar("sergio")
+saludar("rodrigo")
+saludar("juan")
+"""
+
+# celsius a farenhait: (c * 1.8) + 32
+
+def convertir_a_farenheit(c):
+    return (c * 1.8) + 32
+
+print(convertir_a_farenheit(10))
+print(convertir_a_farenheit(30))
+print(convertir_a_farenheit(80))
